5ed25d839b8a56da8e3a55e21ac060941610c220
Alexander Kollan Collin <kollan.alex@gmail.com>
2018-05-11 21:07:08 +0300

    port change

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.PORT || 3000;
+
 
 app.use( express.static( `${__dirname}/build` ) );
 
-app.listen(server_port, server_ip_address, function(){
-    console.log( "Listening on " + server_ip_address + ", port " + server_port )
+app.listen(server_port, function(){
+    console.log( "Listening on  port " + server_port )
 })
 
 
