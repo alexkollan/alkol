1d977126eae1b40ecedcd9bb2b1bb8ea33644c4a
Alexander Kollan Collin <kollan.alex@gmail.com>
2018-05-11 19:39:20 +0300

    RHC connection tweaks

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 app.use( express.static( `${__dirname}/build` ) );
 
-app.listen(5000, () => console.log('Example app listening on port 3000!'))
+app.listen(server_port, server_ip_address, () => console.log( "Listening on " + server_ip_address + ", port " + server_port ))
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'index.html'));
-    console.log('We have 1 visitor!');
-    
 })
